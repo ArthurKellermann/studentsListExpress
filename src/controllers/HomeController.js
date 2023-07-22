36ddbaa4d754434cd4138f0871e602d78ed16159
Arthur Kellermann <arthur.kellermann956@gmail.com>
2023-07-22 18:46:32 -0300

    update home

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -6,43 +6,43 @@ class HomeController {
           user: {
             register: {
               type: 'POST',
-              url: 'http://localhost:3000/users/',
+              url: 'http://localhost:8000/users/',
             },
             update: {
               type: 'PUT',
-              url: 'http://localhost:3000/users/',
+              url: 'http://localhost:8000/users/',
             },
             delete: {
               type: 'DELETE',
-              url: 'http://localhost:3000/users/',
+              url: 'http://localhost:8000/users/',
             },
           },
           auth: {
             login: {
               type: 'POST',
-              url: 'http://localhost:3000/auth/',
+              url: 'http://localhost:8000/auth/',
             },
           },
           students: {
             index: {
               type: 'GET',
-              url: 'http://localhost:3000/students/',
+              url: 'http://localhost:8000/students/',
             },
             store: {
               type: 'POST',
-              url: 'http://localhost:3000/students/',
+              url: 'http://localhost:8000/students/',
             },
             showById: {
               type: 'GET',
-              url: 'http://localhost:3000/students/:id',
+              url: 'http://localhost:8000/students/:id',
             },
             update: {
               type: 'PUT',
-              url: 'http://localhost:3000/students/:id',
+              url: 'http://localhost:8000/students/:id',
             },
             delete: {
               type: 'DELETE',
-              url: 'http://localhost:3000/students/:id',
+              url: 'http://localhost:8000/students/:id',
             },
           },
         },
